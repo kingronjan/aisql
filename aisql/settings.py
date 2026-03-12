bc3a75b967d8bd9292de184969d6397e92dc8d8b
kingron jan <kingronjan@qq.com>
2026-03-12 15:17:44 +0800

    first commit

---
+++ aisql/settings.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    api_key: str
+
+    # 自动读取 .env 文件
+    model_config = SettingsConfigDict(env_file=".env", env_prefix='aisql_')
+
+settings = Settings()
+
